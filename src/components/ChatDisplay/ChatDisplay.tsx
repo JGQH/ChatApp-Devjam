4f975a746570dad9eb268cfbe5c3574b0a36d230
Juan Quispe <jquispeh63@gmail.com>
2021-12-08 08:58:04 -0500

    Add note about names not loading
    After some time of testing, still not all names are loaded on sign up
    Not only on sign up, sometimes when recently signed up user messages too
    Therefore, I just added a message about it and how to "fix it"

--- src/components/ChatDisplay/ChatDisplay.tsx
+++ src/components/ChatDisplay/ChatDisplay.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import GUN, { SEA } from 'gun'
 import Bottomer from '@Components/Bottomer'
 import useGUN from '@Hooks/useGUN'
-import GUN, { SEA } from 'gun'
 import ChatMessage, { MessageInterface } from './ChatMessage'
 import style from './ChatDisplay.module.scss'
 
@@ -10,6 +10,7 @@ const key = import.meta.env.VITE_GUN_KEY as string
 export default function ChatDisplay() {
   const { db, username } = useGUN()
   const [ messages, setMessages ] = useState<MessageInterface[]>([])
+  const [ namesNotLoaded, setNamesNotLoaded ] = useState(false)
 
   useEffect(() => {
     db.get('general').map().once(async (data) => {
@@ -22,24 +23,31 @@ export default function ChatDisplay() {
           time: +GUN.state.is(data, 'content') //Gets the name of the node, which is set to the time of sending
         }
         
-        if(message.content) {
-          setMessages(newMessages => [...newMessages.slice(-100), message]) //Only get the last 100 messages
-        }
+        if(!message.alias) setNamesNotLoaded(true)
+
+        if(message.content) setMessages(newMessages => [...newMessages.slice(-100), message]) //Only get the last 100 messages
       }
       
     } )
 
-    return () => db.off()
+    return () => db.get('general').map().off()
   }, [])
 
   return (
-    <div className={style.container}>
-      {messages.sort((a, b) => a.time - b.time).map((message, key) => {
-        const isUser = message.alias === username
+    <>
+      <div className={style.container}>
+        {messages.sort((a, b) => a.time - b.time).map((message, key) => {
+          const isUser = message.alias === username
 
-        return <ChatMessage {...{...message, isUser, key}}/>
-      })}
-      <Bottomer />
-    </div>
+          return <ChatMessage {...{...message, isUser, key}}/>
+        })}
+        <Bottomer />
+      </div>
+      {namesNotLoaded &&
+      <div className={style.error}>
+        <p>Some names weren't loaded properly, try refreshing the page.</p>
+        <button onClick={() => setNamesNotLoaded(false)}>✖</button>
+      </div>}
+    </>
   )
 }
